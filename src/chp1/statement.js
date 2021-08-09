b18ec0b70b77b977fa02e03fdf082e2deb10ded3
Luca Giovenzana <luca@giovenzana.org>
2021-08-10 00:32:49 +0200

    refactor: Replace Temp with Query `play`

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -2,10 +2,14 @@ export {statement}
 
 function statement(invoice, plays) {
 
-    function amountFor(aPerformance, play) {
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
         let result = 0;
 
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -20,10 +24,11 @@ function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
         return result;
     }
+
 
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -35,16 +40,15 @@ function statement(invoice, plays) {
         }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play)
+        let thisAmount = amountFor(perf, playFor(perf))
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // print line for this order
-        result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
