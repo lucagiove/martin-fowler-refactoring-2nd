95bbb4db713756d7b8a7236e5fde3a7ec1b06cbc
Luca Giovenzana <luca@giovenzana.org>
2021-08-15 22:40:22 +0200

    refactor: 5

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -37,22 +37,27 @@ function statement(invoice, plays) {
         return result
     }
 
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber / 100);
+    }
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format;
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
+    }
 
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    for (let perf of invoice.performances) {
+        result += `  ${playFor(perf).name}: ${usd((amountFor(perf)))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
