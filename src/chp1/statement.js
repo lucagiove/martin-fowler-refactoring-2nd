a15bba6fa248f06ea502229a667cfb68e8a95ea4
Luca Giovenzana <luca@giovenzana.org>
2022-05-27 00:39:11 +0200

    refactor: html statement and fixed file split

--- src/chp1/statement.js
+++ src/chp1/statement.js
@@ -1,66 +1,45 @@
-import {renderPlainText} from "./renderPlainText";
+import {createStatementData} from "./createStatementData";
 
-export {statement}
+export {statement, htmlStatement}
 
 function statement(invoice, plays) {
-    const statementData = {
-        customer: invoice.customer,
-        performances: invoice.performances.map(p => enrichPerformance(p)),
-    }
-    statementData.totalAmount = totalAmount(statementData.performances)
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData.performances)
-
-    return renderPlainText(statementData);
-
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(aPerformance);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result)
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result
-    }
-
-    function totalAmount(performances) {
-        return performances.reduce((total, p) => total + p.amount, 0)
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = Math.max(aPerformance.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
-    function totalVolumeCredits(performances) {
-        return performances.reduce((total, p) => total + p.volumeCredits, 0)
-    }
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
+function renderPlainText(data) {
+    let result = `Statement for ${data.customer}\n`;
 
+    for (let perf of data.performances) {
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+}
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return (new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber / 100));
+}
--- test/chp1/statement.test.js
+++ test/chp1/statement.test.js
@@ -41,18 +41,16 @@ describe('statement', () => {
         expect(statement(invoicesJson[0], playsJson)).to.equal(expected);
     });
 
-    // it('should print a statement for multiple plays, single customer and multiple seats in html', () => {
-    //     let expected  = "<h1>Statement for BigCo</h1>\n" +
-    //         "<table>\n" +
-    //         "<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n" +
-    //         "  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n" +
-    //         "  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n" +
-    //         "</table>\n" +
-    //         "<p>Amount owed is <em>$1,730.00</em></p>\n"+
-    //         "<p>You earned <em>47</em> credits</p>\n";
-    //
-    //     expect(htmlStatement(invoicesJson[0], playsJson)).to.equal(expected);
-    // });
+    it('should print a statement for multiple plays, single customer and multiple seats in html', () => {
+        let expected  = "<h1>Statement for BigCo</h1>\n" +
+            "<table>\n" +
+            "<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n" +
+            "  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n" +
+            "  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n" +
+            "</table>\n" +
+            "<p>Amount owed is <em>$1,730.00</em></p>\n"+
+            "<p>You earned <em>47</em> credits</p>\n";
 
-
+        expect(htmlStatement(invoicesJson[0], playsJson)).to.equal(expected);
+    });
 });
